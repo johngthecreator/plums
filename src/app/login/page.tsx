936bf1f65608f99b862d984c44617a25358a8370
SecretDwarf <briggsjjacob@gmail.com>
2024-02-03 22:44:25 -0700

    styled login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,21 @@
+// pages/login.tsx
+"use client"
+
+export default function LoginPage() {
+  const handleSubmit = () => {
+    alert("Sign in button pressed");
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-[#6E43B1] to-white">
+      <div className="p-8 text-gray-200 text-center">
+        <h1 className="text-6xl font-semibold mb-4">Plums</h1>
+        <p className="mb-4">Rethink your personal learning.</p>
+        <button className="text-2xl font-semibold mb-4 rounded-full border-4 border-gray-200 px-4 py-2 text-gray-200 hover:bg-gray-200 hover:text-[#FAABBA] focus:outline-none focus:bg-gray-200 focus:text-purple-800" onClick={handleSubmit}>
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
+}
+
