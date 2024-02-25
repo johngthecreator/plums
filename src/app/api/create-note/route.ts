f1ac128c430ec2e8e5bdc29c0f43e6fb60e9535d
Shelby Vasas <shelbyvasas@gmail.com>
2024-02-24 22:41:40 -0700

    update create note

--- src/app/api/create-note/route.ts
+++ src/app/api/create-note/route.ts
@@ -5,16 +5,16 @@ const prisma = new PrismaClient()
 export async function POST(req: Request) {
   const body = await req.json();
   try {
-    const newSpace = await prisma.note.create({
+    const newNote = await prisma.note.create({
       data: {
         spaceId: body.spaceId,
         title: body.title,
         data:body.data,
       },
     });
-    return Response.json(newSpace);
+    return Response.json(newNote);
   } catch (error) {
     console.error('Request error', error);
-    return Response.json({ error: 'Error creating space', success: false });
+    return Response.json({ error: 'Error creating note', success: false });
   }
 } 
